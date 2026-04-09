5df710af454bda4e0b741921b7eb1d52192f9877
GojoSatoru1738 <ajmiller@email.neit.edu>
2026-04-09 12:38:48 -0400

    ye

--- js/GameObject.js
+++ js/GameObject.js
@@ -4,23 +4,18 @@ function GameObject() {
     this.width = 50;
     this.height = 50;
     this.color = "#00ff00";
-    this.vx = 7;
-    this.vy = 7;
+    this.vx = 20;
+    this.vy = 20;
 //ye
     this.drawCircle = function() {
-        context.save();
-        context.fillStyle = this.color;
-        context.beginPath();
-        context.arc(this.x, this.y, this.width/2, 0, 360 * Math.PI/180, true);
-        context.closePath();
-        context.fill();
-        context.restore();
-    };
-    //YUH
-    this.move = function() {
-        this.x += this.vx;
-        this.y += this.vy;
+
+        
     };
 
+    this.drawRect = function() {
+       
+        
+    };
+    
    
 }
